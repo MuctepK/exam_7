9dbe18f7a050eac557e881a3c2ef38058c419878
Kutman <muctep.k@gmail.com>
2019-10-19 11:44:44 +0600

    Создал модели вопроса и выбора ответов.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=128, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.question[:30]
+
+
+class Choice(models.Model):
+    answer = models.CharField(max_length=128, verbose_name='Ответ')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='choices')
